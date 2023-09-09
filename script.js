11d671bdb2c48f470b648e1b1534d9c454043a6d
Artak Navoyan <navoyan.ab@gmail.com>
2023-09-09 20:47:31 +0200

    fixed mobile menu

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Get all the nav links
+// smoot scroll
 const navLinks = document.querySelectorAll('.nav-link');
 
-// Add a click event listener to each nav link
 navLinks.forEach(link => {
    link.addEventListener('click', event => {
-      // Prevent the default behavior of the link
+
       event.preventDefault();
 
-      // Get the target element from the link's href attribute
       const targetId = link.getAttribute('href');
       const targetElement = document.querySelector(targetId);
 
-      // Scroll the target element into view with smooth behavior
       targetElement.scrollIntoView({ behavior: 'smooth' });
    });
 });
 
-// Get Hamburger element
+// Mobile menu
 const hamburger = document.querySelector('.header__hamburger')
-const webMenu = document.querySelector('.header__webMenu')
 const mobileMenu = document.querySelector('.header__mobileMenu')
 const closeMenu = document.querySelector('.header__closeMobileMenu')
 const mobileMenuItem = document.querySelectorAll('.header__mobileMenuItem')
 
-hamburger.addEventListener('click', () => {
+function openMobileMenu() {
    mobileMenu.classList.add('is-active')
-});
+}
 
-closeMenu.addEventListener('click', () => {
+function closeMobileMenu() {
    mobileMenu.classList.remove('is-active')
-});
+}
 
-// mobileMenuItem.forEach(item => {
-//    item.addEventListener('click', () => {
-//       mobileMenu.style.display = 'none';
-//    })
-// })
+hamburger.addEventListener('click', openMobileMenu);
 
-// window.addEventListener('resize', () => {
-//    if (window.innerWidth >= 768) {
-//       mobileMenu.style.display = 'none';
-//    }
-// });
+closeMenu.addEventListener('click', closeMobileMenu);
 
+mobileMenuItem.forEach(item => {
+   item.addEventListener('click', closeMobileMenu)
+})
